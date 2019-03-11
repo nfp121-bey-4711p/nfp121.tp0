699b48a8f95f36943d34dccb26792897a69e158a
joe sfeir <joe.sfeir@isae.edu.lb>
2019-03-11 19:27:44 +0200

    Mon Vrai premier Push

--- premierEssai/A.java
+++ premierEssai/A.java
@@ -5,7 +5,7 @@
  * @author (votre nom)
  * @version (un numéro de version ou une date)
  */
-public class A
+public class A extends TestGIT
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
     private int x;
